2273ba6a57b82f7aac116e2d3603c0dc6e825fd4
Jian-You Lin <jianyoulin@gmail.com>
2025-01-22 12:00:18 -0500

    fixed bugs (.lower to .lower())

--- pi_rig.py
+++ pi_rig.py
@@ -9,23 +9,24 @@ import time
 import RPi.GPIO as GPIO
 import os
 
-def laser_test(pin, dur = 20):
+def laser_test(pin=25, dur = 20):
     """
     pin as an integer
     """
 
     # Setup pi board GPIO ports
     GPIO.setmode(GPIO.BCM)
+    GPIO.cleanup()
     GPIO.setup(pin, GPIO.OUT) 
     run_laser = True
-
+    
     while run_laser:
         GPIO.output(pin, 1)
         time.sleep(dur)
         GPIO.output(pin, 0)
         time.sleep(1)
         re_run = input('Do you want to re-run the laser (Y or N)?')
-        if re_run[0].lower == 'n':
+        if re_run[0].lower() == 'n':
             run_laser = False
 
-    print('Fiber has been measured.')
+    print('Fiber output power has been measured.')
